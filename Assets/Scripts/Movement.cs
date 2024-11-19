99ae64882bdffe73db92bad7440db0fbb4e0e8dc
saminame <151252937+saminame@users.noreply.github.com>
2024-11-18 20:09:06 -0800

    added sound implementation for player actions
    Pushed by Samina Esteqlal and Hasina Esteqlal (Hasina was having issues with her Unity so she worked on it on mine.)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -18,6 +18,16 @@ public class Movement : MonoBehaviour
     public TMP_Text loseText;
     public Button retryButton;
 
+    public AudioSource audioSource;  // Reference to AudioSource component
+    public AudioClip walkingSound;   // Walking sound effect
+    public AudioClip canePickupSound; // Cane pick up sound effect
+    public AudioClip winSound;        // Win sound effect
+    public AudioClip loseSound;       // Lose sound effect
+    public AudioClip reminderSound;   // Reminder sound effect
+
+    private bool isWalking = false;   // Keep track of whether the player is walking
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,6 +41,21 @@ public class Movement : MonoBehaviour
         {
             Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
             controller.Move(move * Time.deltaTime * movementSpeed);
+
+            if (move.magnitude > 0 && !isWalking)
+            {
+                // Player has started moving
+                isWalking = true;
+                audioSource.clip = walkingSound;
+                audioSource.loop = true;  // Loop the walking sound while the player is moving
+                audioSource.Play();
+            }
+            else if (move.magnitude == 0 && isWalking)
+            {
+                // Player has stopped moving
+                isWalking = false;
+                audioSource.Stop();
+            }
         }
     }
 
@@ -42,6 +67,7 @@ public class Movement : MonoBehaviour
             StartCoroutine(DisplayText(caneText, 3f));
 
             // Play some sort of pick up sound
+            audioSource.PlayOneShot(canePickupSound);
             Debug.Log("Pickup Cane");
         }
         else if(collision.transform.CompareTag("CampSite"))
@@ -51,12 +77,14 @@ public class Movement : MonoBehaviour
                 StartCoroutine(DisplayText(winText, 60f));
                 retryButton.gameObject.SetActive(true);
                 // Play a win sound
+                audioSource.PlayOneShot(winSound);
                 Debug.Log("Win!");
             }
             else
             {
                 StartCoroutine(DisplayText(needCaneText, 3f));
                 // Play some beep sound or something
+                audioSource.PlayOneShot(reminderSound);
                 Debug.Log("Get cane first");
             }
         }
@@ -67,6 +95,7 @@ public class Movement : MonoBehaviour
             playerRenderer.enabled = false;
 
             // Maybe play a sound of bear or player dying lol
+            audioSource.PlayOneShot(loseSound);
             Debug.Log("You died");
         }
     }
